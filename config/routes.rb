7bee49ec32d09d978ab3c07726a5ace02ee43dea
shiftky <shiftky@gmail.com>
2013-03-27 20:39:55 +0900

    ルーティングの修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 HaimsRails::Application.routes.draw do
+  get "device/index"
+
+  get "device/show"
+
   # devise
   devise_for :users
 
@@ -10,5 +14,10 @@ HaimsRails::Application.routes.draw do
   namespace :management do
     # マイコンボード
     resources :board, :defaults => { :format => 'html' }
+    # 制御対象機器一覧
+    resources :device, :defaults => { :format => 'html' }, :except => [:edit, :update] do
+      # 赤外線リモコンの信号
+      resources :ir_signal, :defaults => { :format => 'html' }, :except => [:edit, :update]
+    end
   end
 end
---
+++ test/unit/helpers/management/device_helper_test.rb
@@ -0,0 +1,4 @@
+require 'test_helper'
+
+class Management::DeviceHelperTest < ActionView::TestCase
+end
