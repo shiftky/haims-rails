ecb6462cba45b038cd7c9bfbb0c839beaec40369
shiftky <shiftky@gmail.com>
2013-03-28 22:40:59 +0900

    現在の気温・照度の表示に対応

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,20 @@
 class HomeController < ApplicationController
   def index
+    board = Board.where("name = ?", "main")
+    sp = SerialPort.new(board.first.serial_port, 9600, 8, 1, SerialPort::NONE)
+    sleep 1
+    sp.puts "2\n"
+    while sp.gets do
+      @lx = sp.gets
+      break
+    end
+
+    sp.puts "3\n"
+    while sp.gets do
+      @temp = sp.gets
+      break
+    end
+
+    sp.close
   end
 end
