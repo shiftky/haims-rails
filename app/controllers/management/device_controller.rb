601b10ef793a4cb7b4c270c73a35f7d5172c70e1
shiftky <shiftky@gmail.com>
2013-03-28 09:54:46 +0900

    制御対象機器一覧ページを追加

--- app/controllers/management/device_controller.rb
+++ app/controllers/management/device_controller.rb
@@ -1,7 +1,30 @@
+# encoding: utf-8
+
 class Management::DeviceController < ApplicationController
   def index
+    @devices = Device.all
   end
 
   def show
+    @device = Device.find(params[:id])
+  end
+
+  def new
+    @device = Device.new
+  end
+
+  def create
+    @device = Device.new(params[:device])
+
+    if @device.save
+      redirect_to management_device_index_path, :notice => "登録しました"
+    else
+      redirect_to edit_management_device_index_path, :notice => "登録に失敗しました"
+    end
+  end
+
+  def destroy
+    Board.find(params[:id]).destroy
+    redirect_to management_device_index_path, :notice => "削除しました"
   end
 end
