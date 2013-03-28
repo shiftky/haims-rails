5574422da6a35a6f909ea975904a358c1bf2630f
shiftky <shiftky@gmail.com>
2013-03-28 10:54:35 +0900

    ルーティングの修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 HaimsRails::Application.routes.draw do
+  get "ir_signal/new"
+
   get "device/index"
 
   get "device/show"
@@ -17,7 +19,7 @@ HaimsRails::Application.routes.draw do
     # 制御対象機器一覧
     resources :device, :defaults => { :format => 'html' }, :except => [:edit, :update] do
       # 赤外線リモコンの信号
-      resources :ir_signal, :defaults => { :format => 'html' }, :except => [:edit, :update]
+      resources :ir_signal, :defaults => { :format => 'html' }, :except => [:index, :show, :edit, :update]
     end
   end
 end
